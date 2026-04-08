950a8e2d9f0c97d0234db2f0cf7593d0741acb90
Vinícius Lessa <vinicius.lessa33@outlook.com>
2026-04-08 20:34:42 -0300

    feat: serveStatic js for static frontend files

---
+++ utils/serveStatic.js
@@ -0,0 +1,32 @@
+import path from 'node:path'
+import fs from 'node:fs/promises'
+import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
+
+export async function serveStatic(req, res, baseDir) {
+  const publicDir = path.join(baseDir, 'public')
+  const filePath = path.join(
+    publicDir,
+    req.url === '/' ? 'index.html' : req.url
+  )
+
+  const ext = path.extname(filePath)
+
+  const contentType = getContentType(ext)
+
+  try {
+    const content = await fs.readFile(filePath)
+    sendResponse(res, 200, contentType, content)
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      const notFoundPath = path.join(publicDir, '404.html')
+      const content = await fs.readFile(notFoundPath)
+      sendResponse(res, 404, 'text/html', content)
+    } 
+    else {
+      sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err.code}</h1></html>`)
+    }
+  }
+
+}
